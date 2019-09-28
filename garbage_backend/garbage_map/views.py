599bae8af0acb22cd6fbb0087de22b36b9b53ec5
David Pribil <szamuran@gmail.com>
2019-09-28 10:43:05 +0200

    Added polyline support. Fixed setting view bounds

--- garbage_backend/garbage_map/views.py
+++ garbage_backend/garbage_map/views.py
@@ -6,5 +6,12 @@ import json
 
 def index(request):
     template = loader.get_template("garbage_map/index.html")
-    context = {"polygon": json.dumps(ROI.objects.first().polygon.coords)}
+    polygons = []
+    for roi in ROI.objects.all():
+        if roi.geometry == "Polygon":
+            points = roi.polygon.coords
+        else:
+            points = roi.line_string.coords
+        polygons.append({"geometry": roi.geometry, "points": json.dumps(points)})
+    context = {"polygons": polygons}
     return HttpResponse(template.render(context, request))
--- garbage_backend/static/garbage_map/app.js
+++ garbage_backend/static/garbage_map/app.js
@@ -1,5 +1,32 @@
-function displayPolygon(map, latlngs) {
-    var polygon = L.polygon(latlngs, { color: 'red' }).addTo(map);
-    // zoom the map to the polygon
-    map.fitBounds(polygon.getBounds());
+class RoiCollector {
+
+    constructor(map) {
+        this.map = map
+        this.bounds = null
+    }
+
+    displayPolygon(latlngs) {
+        var polygon = L.polygon(latlngs, { color: 'red' }).addTo(this.map);
+        this.handleBounds(polygon)
+    }
+
+    displayPolyLine(latlngs) {
+        var polyLine = L.polyline(latlngs, { color: 'red' }).addTo(this.map);
+        this.handleBounds(polyLine)
+    }
+
+    handleBounds(poly) {
+        let bounds = poly.getBounds();
+        if (this.bounds === null) {
+            this.bounds = bounds;
+        } else {
+            this.bounds.extend(bounds);
+        }
+    }
+
+    centerMap() {
+        this.map.maxZoom = 15;
+        this.map.fitBounds(this.bounds);
+    }
+
 }
